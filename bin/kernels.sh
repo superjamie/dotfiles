66306462e01abcae835b21e52699e4bb8cb9439c
Jamie Bainbridge <jamie.bainbridge@gmail.com>
2019-11-12 20:31:48 +1000

    Update kernels.sh

--- bin/kernels.sh
+++ bin/kernels.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-# list installed kernels on ubuntu so you can erase old ones
+# list installed kernels on detected distros so you can erase old ones
 
-sudo apt list --installed 2>/dev/null | awk -F'/' '/^linux.*(header|image|module)/{print $1}' | sort
+# /etc/os-release is present on Ubu 18.04 and CentOS 7, but not on CentOS 6
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    if [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
+        sudo apt list --installed 2>/dev/null | awk -F'/' '/^linux.*(header|image|module)/{print $1}' | sort
+    elif [[ "$ID" == "centos" || "$ID" == "rhel" || "$ID" == "fedora" ]]; then
+        sudo rpm -qa | egrep "^kernel" | sort
+    fi  
+fi
