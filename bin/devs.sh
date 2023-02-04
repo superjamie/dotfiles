8992505e742b7b95000bedd56e51b18e562e4bd1
Jamie Bainbridge <jamie.bainbridge@gmail.com>
2023-02-04 22:57:32 +1100

    devs: use proper lsblk output formatting

--- bin/devs.sh
+++ bin/devs.sh
@@ -2,4 +2,5 @@
 # lists block devices
 # so you don't erase the wrong one when managing usb drive or sdcard
 
-lsblk | awk '/sd[a-z][^0-9]/ {print $1 " " $4}'
+#lsblk | awk '/sd[a-z][^0-9]/ {print $1 " " $4}'
+lsblk --exclude 7,11 --output NAME,SIZE,LABEL
